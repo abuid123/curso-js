6eff7d320b8a23f2b5d879aa718db6b5f46bdc84
Agustin Fernandez Casa <agusfernandez2512@gmail.com>
2022-06-02 22:57:31 -0300

    nada

--- react/mi-app/src/index.js
+++ react/mi-app/src/index.js
@@ -1,17 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-//import App from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-const X = ()=><p>Hola Mundo</p>
-root.render(<X />)
+ root.render(
+   <React.StrictMode>
+     <App />
+   </React.StrictMode>
+ );
+// const Li = ({children, estado,casa,edad})=> {
+//     console.log(casa,edad)
+//     return (
+//     <li>{children} estoy {estado}</li>
+//     )
+// }
+// const X = ()=>
+//     <ul>
+//         <Li
+//         estado={'SemiFeliz'}
+//         casa={false}
+//         edad={24}
+//         >
+//             Chanchito
+//         </Li>
+//         <Li estado = {'triste'}>Chanchito</Li>
+//         <Li estado={'emocionado'}>Felipe</Li>
+//     </ul>
+
+// root.render(<X />)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
