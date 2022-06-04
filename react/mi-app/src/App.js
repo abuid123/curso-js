2f68b63cfbf0f82001f0235842269696db3ee2d7
Agustin <agusfernandez2512@gmail.com>
2022-06-03 23:24:18 -0300

    modificando la app

--- react/mi-app/src/App.js
+++ react/mi-app/src/App.js
@@ -1,10 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 
+const estilo = (bg = '#333')=>({
+  backgroundColor: bg,
+  color:'#fff',
+  padding:'5px 5px',
+  margin: '5px 5px',
+})
+
+const Li = (props)=>{
+  console.log(props)
+  return(
+    <li style={estilo('#000')} className='clase-li'>Valor de li</li>
+  )
+}
+
 function App() {
   const valor = 'Triste';
   return (
-    <p>Chanchito {valor}</p>
+    <ul className='clase-css'>
+      <Li></Li>
+    </ul>
   );
 }
 
